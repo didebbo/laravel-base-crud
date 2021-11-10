483d87e8d6966dcdb65628886fbced53c177ef8d
didebbo <gianluca.napoletano.93@gmail.com>
2021-11-10 16:57:29 +0100

    aggiunto create e post

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -11,27 +11,31 @@
                 @csrf
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" class="form-control" id="title">
+                    <input type="text" class="form-control" id="title" name="title">
                 </div>
                 <div class="form-group">
                     <label for="description">Description</label>
-                    <input type="text" class="form-control" id="description">
+                    <input type="text" class="form-control" id="description" name="description">
+                </div>
+                <div class="form-group">
+                    <label for="thumb">Thumb</label>
+                    <input type="text" class="form-control" id="thumb" name="thumb">
                 </div>
                 <div class="form-group">
                     <label for="price">Price</label>
-                    <input type="text" class="form-control" id="price">
+                    <input type="text" class="form-control" id="price" name="price">
                 </div>
                 <div class="form-group">
                     <label for="series">Series</label>
-                    <input type="text" class="form-control" id="series">
+                    <input type="text" class="form-control" id="series" name="series">
                 </div>
                 <div class="form-group">
                     <label for="sale_date">Sale Date</label>
-                    <input type="date" class="form-control" id="sale_date">
+                    <input type="date" class="form-control" id="sale_date" name="sale_date">
                 </div>
                 <div class="form-group">
                     <label for="type">Type</label>
-                    <input type="text" class="form-control" id="type">
+                    <input type="text" class="form-control" id="type" name="type">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
