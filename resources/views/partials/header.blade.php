ee360a7ee501cf5ec1c91ebe37c8afda1faf40f4
didebbo <gianluca.napoletano.93@gmail.com>
2021-11-10 17:13:14 +0100

    refactoring css

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        <a class="navbar-brand" href="{{ route('index') }}">Home</a>
+        {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -34,6 +34,6 @@
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
-        </div>
+        </div> --}}
     </nav>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,3 +3,27 @@
 @section('title')
     Home
 @endsection
+
+@section('main')
+    <main>
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Entity</th>
+                        <th scope="col">Actions</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td><a href="{{ route('comics.index') }}">Comics</a></td>
+                        <td><a href="{{ route('comics.create') }}">New</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </main>
+@endsection
